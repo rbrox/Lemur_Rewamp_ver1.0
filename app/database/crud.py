922e27d11c3d02768c6f1c89c61aef4b71e3aa81
Rishav Bhagat <rishavbhagat@Rishavs-MacBook-Air.local>
2025-06-18 13:00:10 +0530

    feat: added dummy UI for th project

--- app/database/crud.py
+++ app/database/crud.py
@@ -3,14 +3,14 @@ from sqlalchemy.orm import Session
 from app.database.models.user import User
 
 
-def get_user(db: Session, user_id: int):
+def get_user(db: Session, user_id: int) -> User:
     return db.query(User).filter(User.id == user_id).first()
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
-def get_users(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(User).offset(skip).limit(limit).all()
+def get_users(db: Session, skip: int = 0) -> list[User]:
+    return db.query(User).offset(skip).all()
 
 def create_user(db: Session, name: str, email: str, password: str):
     db_user = User(name=name, email=email, password=password)
